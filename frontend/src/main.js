60fc6a607acc26aa9c0d26f8420854b5ce020906
a85009456 <a85009546@gmail.com>
2025-01-01 23:23:24 +0800

    feat: 文章管理 - 頁面搭建

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,8 @@ import 'element-plus/dist/index.css'
 import router from '@/router'
 import { createPinia } from 'pinia'
 import { createPersistedState } from 'pinia-persistedstate-plugin'
+import locale from 'element-plus/dist/locale/zh-cn.js'
+
 
 
 import App from './App.vue'
@@ -17,4 +19,5 @@ pinia.use(persist);
 app.use(pinia);
 app.use(ElementPlus);
 app.use(router);
+app.use(ElementPlus, { locale })
 app.mount('#app')
--- frontend/src/views/article/ArticleManage.vue
+++ frontend/src/views/article/ArticleManage.vue
@@ -1,3 +1,163 @@
+<script setup>
+import {
+    Edit,
+    Delete
+} from '@element-plus/icons-vue'
+
+import { ref } from 'vue'
+
+//文章分類數據模型
+const categorys = ref([
+    {
+        "id": 3,
+        "categoryName": "美食",
+        "categoryAlias": "my",
+        "createTime": "2023-09-02 12:06:59",
+        "updateTime": "2023-09-02 12:06:59"
+    },
+    {
+        "id": 4,
+        "categoryName": "娱乐",
+        "categoryAlias": "yl",
+        "createTime": "2023-09-02 12:08:16",
+        "updateTime": "2023-09-02 12:08:16"
+    },
+    {
+        "id": 5,
+        "categoryName": "軍事",
+        "categoryAlias": "js",
+        "createTime": "2023-09-02 12:08:33",
+        "updateTime": "2023-09-02 12:08:33"
+    }
+])
+
+//用戶搜索時選中的分類id
+const categoryId=ref('')
+
+//用戶搜索時選中的發布狀態
+const state=ref('')
+
+//文章列表數據模型
+const articles = ref([
+    {
+        "id": 5,
+        "title": "陕西旅游攻略",
+        "content": "兵马俑,华清池,法门寺,华山...爱去哪去哪...",
+        "coverImg": "https://big-event-gwd.oss-cn-beijing.aliyuncs.com/9bf1cf5b-1420-4c1b-91ad-e0f4631cbed4.png",
+        "state": "草稿",
+        "categoryId": 2,
+        "createTime": "2023-09-03 11:55:30",
+        "updateTime": "2023-09-03 11:55:30"
+    },
+    {
+        "id": 5,
+        "title": "陕西旅游攻略",
+        "content": "兵马俑,华清池,法门寺,华山...爱去哪去哪...",
+        "coverImg": "https://big-event-gwd.oss-cn-beijing.aliyuncs.com/9bf1cf5b-1420-4c1b-91ad-e0f4631cbed4.png",
+        "state": "草稿",
+        "categoryId": 2,
+        "createTime": "2023-09-03 11:55:30",
+        "updateTime": "2023-09-03 11:55:30"
+    },
+    {
+        "id": 5,
+        "title": "陕西旅游攻略",
+        "content": "兵马俑,华清池,法门寺,华山...爱去哪去哪...",
+        "coverImg": "https://big-event-gwd.oss-cn-beijing.aliyuncs.com/9bf1cf5b-1420-4c1b-91ad-e0f4631cbed4.png",
+        "state": "草稿",
+        "categoryId": 2,
+        "createTime": "2023-09-03 11:55:30",
+        "updateTime": "2023-09-03 11:55:30"
+    },
+])
+
+//分頁條數據模型
+const pageNum = ref(1)//當前頁
+const total = ref(20)//總條數
+const pageSize = ref(3)//每頁條數
+
+//當每頁條數發生了變化，調用此函数
+const onSizeChange = (size) => {
+    pageSize.value = size
+}
+//當前頁碼發生變化，調用此函数
+const onCurrentChange = (num) => {
+    pageNum.value = num
+}
+
+// 回顯文章分類
+import { articleCategoryListService } from '@/api/article.js'
+const articleCategoryList = async () => {
+    let result = await articleCategoryListService();
+    categorys.value = result.data;
+};
+articleCategoryList();
+</script>
 <template>
-    文章管理
+    <el-card class="page-container">
+        <template #header>
+            <div class="header">
+                <span>文章管理</span>
+                <div class="extra">
+                    <el-button type="primary">添加文章</el-button>
+                </div>
+            </div>
+        </template>
+        <!-- 搜索表單 -->
+        <el-form inline>
+            <el-form-item label="文章分類：">
+                <el-select placeholder="請選擇" v-model="categoryId">
+                    <el-option 
+                        v-for="c in categorys" 
+                        :key="c.id" 
+                        :label="c.categoryName"
+                        :value="c.id">
+                    </el-option>
+                </el-select>
+            </el-form-item>
+
+            <el-form-item label="發布狀態：">
+                <el-select placeholder="請選擇" v-model="state">
+                    <el-option label="已發布" value="已發布"></el-option>
+                    <el-option label="草稿" value="草稿"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary">搜索</el-button>
+                <el-button>重置</el-button>
+            </el-form-item>
+        </el-form>
+        <!-- 文章列表 -->
+        <el-table :data="articles" style="width: 100%">
+            <el-table-column label="文章標題" width="400" prop="title"></el-table-column>
+            <el-table-column label="分類" prop="categoryId"></el-table-column>
+            <el-table-column label="發表時間" prop="createTime"> </el-table-column>
+            <el-table-column label="狀態" prop="state"></el-table-column>
+            <el-table-column label="操作" width="100">
+                <template #default="{ row }">
+                    <el-button :icon="Edit" circle plain type="primary"></el-button>
+                    <el-button :icon="Delete" circle plain type="danger"></el-button>
+                </template>
+            </el-table-column>
+            <template #empty>
+                <el-empty description="沒有數據" />
+            </template>
+        </el-table>
+        <!-- 分頁條 -->
+        <el-pagination v-model:current-page="pageNum" v-model:page-size="pageSize" :page-sizes="[3, 5 ,10, 15]"
+            layout="jumper, total, sizes, prev, pager, next" background :total="total" @size-change="onSizeChange"
+            @current-change="onCurrentChange" style="margin-top: 20px; justify-content: flex-end" />
+    </el-card>
 </template>
+<style lang="scss" scoped>
+.page-container {
+    min-height: 100%;
+    box-sizing: border-box;
+
+    .header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+}
+</style>
