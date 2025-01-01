90cdbd400b6e4ae76f5b30a1ad3f15eafa2d5d4f
a85009456 <a85009546@gmail.com>
2025-01-01 19:24:14 +0800

    refactor: 加入axios請求攔截器，簡化請求作業

--- frontend/src/api/article.js
+++ frontend/src/api/article.js
@@ -1,9 +1,6 @@
 import request from '@/utils/request.js'
-import { useTokenStore } from '@/stores/token.js'
 
 // 文章分類列表查詢
 export const articleCategoryListService = () => {
-  const tokenStore = useTokenStore();
-  // 在pinia中定義的響應式數據，都不需要 .value
-  return request.get('/category', {headers: {'Authorization':tokenStore.token}})
+  return request.get('/category')
 }
--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -9,6 +9,24 @@ import { ElMessage } from 'element-plus'
 const baseURL = '/api';
 const instance = axios.create({baseURL})
 
+import { useTokenStore } from '@/stores/token.js';
+// 添加請求攔截器
+instance.interceptors.request.use(
+    config=>{
+        // 在發送請求前做甚麼
+        // 添加 token
+        const tokenStore = useTokenStore();
+        // 判斷有無 token
+        if(tokenStore.token){
+            config.headers.Authorization = tokenStore.token;
+        }
+        return config;
+    },
+    err=>{
+        // 如果請求錯誤做甚麼
+        Promise.reject(err);
+    }
+)
 
 // 添加響應攔截器
 instance.interceptors.response.use(
