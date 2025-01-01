ad2149e36d57a329023f1a560a561842f49de1bc
a85009456 <a85009546@gmail.com>
2025-01-01 14:21:01 +0800

    feat: 主頁面搭建

---
+++ frontend/src/views/Layout.vue
@@ -0,0 +1,146 @@
+<script setup>
+import {
+    Management,
+    Promotion,
+    UserFilled,
+    User,
+    Crop,
+    EditPen,
+    SwitchButton,
+    CaretBottom
+} from '@element-plus/icons-vue'
+import avatar from '@/assets/default.png'
+</script>
+
+<template>
+    <!-- element-plus中的容器 -->
+    <el-container class="layout-container">
+        <!-- 左側菜單 -->
+        <el-aside width="200px">
+            <div class="el-aside__logo"></div>
+            <!-- element-plus的菜單標籤 -->
+            <el-menu active-text-color="#ffd04b" background-color="#232323"  text-color="#fff"
+                router>
+                <el-menu-item >
+                    <el-icon>
+                        <Management />
+                    </el-icon>
+                    <span>文章分類</span>
+                </el-menu-item>
+                <el-menu-item >
+                    <el-icon>
+                        <Promotion />
+                    </el-icon>
+                    <span>文章管理</span>
+                </el-menu-item>
+                <el-sub-menu >
+                    <template #title>
+                        <el-icon>
+                            <UserFilled />
+                        </el-icon>
+                        <span>個人中心</span>
+                    </template>
+                    <el-menu-item >
+                        <el-icon>
+                            <User />
+                        </el-icon>
+                        <span>基本資料</span>
+                    </el-menu-item>
+                    <el-menu-item >
+                        <el-icon>
+                            <Crop />
+                        </el-icon>
+                        <span>更換頭像</span>
+                    </el-menu-item>
+                    <el-menu-item >
+                        <el-icon>
+                            <EditPen />
+                        </el-icon>
+                        <span>重置密碼</span>
+                    </el-menu-item>
+                </el-sub-menu>
+            </el-menu>
+        </el-aside>
+        <!-- 右側主區域 -->
+        <el-container>
+            <!-- 頭部區域 -->
+            <el-header>
+                <div>黑馬程序員：<strong>東哥</strong></div>
+                <!-- 下拉菜單 -->
+                <el-dropdown placement="bottom-end">
+                    <span class="el-dropdown__box">
+                        <el-avatar :src="avatar" />
+                        <el-icon>
+                            <CaretBottom />
+                        </el-icon>
+                    </span>
+                    <template #dropdown>
+                        <el-dropdown-menu>
+                            <el-dropdown-item command="profile" :icon="User">基本資料</el-dropdown-item>
+                            <el-dropdown-item command="avatar" :icon="Crop">更換頭像</el-dropdown-item>
+                            <el-dropdown-item command="password" :icon="EditPen">重置密碼</el-dropdown-item>
+                            <el-dropdown-item command="logout" :icon="SwitchButton">退出登入</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </template>
+                </el-dropdown>
+            </el-header>
+            <!-- 中間區域 -->
+            <el-main>
+                <div style="width: 1290px; height: 570px;border: 1px solid red;">
+                    内容展示區
+                </div>
+            </el-main>
+            <!-- 底部區域 -->
+            <el-footer>大事件 ©2023 Created by 黑馬程序員</el-footer>
+        </el-container>
+    </el-container>
+</template>
+
+<style lang="scss" scoped>
+.layout-container {
+    height: 100vh;
+
+    .el-aside {
+        background-color: #232323;
+
+        &__logo {
+            height: 120px;
+            background: url('@/assets/logo.png') no-repeat center / 120px auto;
+        }
+
+        .el-menu {
+            border-right: none;
+        }
+    }
+
+    .el-header {
+        background-color: #fff;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .el-dropdown__box {
+            display: flex;
+            align-items: center;
+
+            .el-icon {
+                color: #999;
+                margin-left: 10px;
+            }
+
+            &:active,
+            &:focus {
+                outline: none;
+            }
+        }
+    }
+
+    .el-footer {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 14px;
+        color: #666;
+    }
+}
+</style>
