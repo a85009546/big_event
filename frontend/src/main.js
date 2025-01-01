1aedaab89c32be19951d6d3d49377992b5fe4a3b
a85009456 <a85009546@gmail.com>
2025-01-01 19:44:19 +0800

    fix: 使用Pinia持久化插件 - persist，解決頁面刷新後數據丟失問題

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,16 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import { router } from '@/router'
-import {createPinia} from 'pinia'
+import { createPinia } from 'pinia'
+import { createPersistedState } from 'pinia-persistedstate-plugin'
 
 
 import App from './App.vue'
 
 const app = createApp(App);
 const pinia = createPinia();
+const persist = createPersistedState();
+pinia.use(persist);
 app.use(pinia);
 app.use(ElementPlus);
 app.use(router);
