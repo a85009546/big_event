d9702984cbbb7bd57777677c9be81e3e5580537e
a85009456 <a85009546@gmail.com>
2025-01-02 13:10:58 +0800

    feat: 基本資料修改

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -25,3 +25,8 @@ export const userLoginService = (loginData) => {
 export const userInfoService = () => {
   return request.get('/user/userInfo')
 }
+
+// 修改用戶信息
+export const userInfoUpdateService = (userInfoData) => {
+  return request.put('/user/update', userInfoData)
+}
